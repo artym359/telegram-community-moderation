2c31e745d93e0b14534d96865f94d4ce5cfee2c7
Artem Churkin <artym359@gmail.com>
2025-02-13 03:46:08 +0300

    Reconstruct moderation handlers: community_moderation/app/checker/functions.py

---
+++ community_moderation/app/checker/functions.py
@@ -0,0 +1,23 @@
+from aiogram.types import Message
+
+from ...bot_instance import bot_admin
+from . import inline_keyboards as ikb
+from ...database import requests as rq
+
+async def next_acc(chat_id: int):
+    acc = await rq.get_New()
+
+    if acc:
+        kb = await ikb.clear_ban_kb(tg_id=acc.tg_id, chat_id=chat_id, message_id=acc.message_id)
+        chat = await bot_admin.get_chat(acc.tg_id)
+        # print(chat.full_name)
+        await bot_admin.send_message(
+            text=f'@{chat.username}\n{chat.full_name}\n{chat.bio}\n{chat.linked_chat_id}',
+            reply_markup=kb,
+            chat_id=chat_id,
+        )
+    else:
+        await bot_admin.send_message(
+            text="Не осталось непроверенных анкет, проверьте позже",
+            chat_id=chat_id,
+        )
